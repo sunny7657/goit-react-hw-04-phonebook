620fadf84dbfdb1f5d6db86a3430b32bcaa6e412
sunny7657 <ksvetlana466@gmail.com>
2024-01-15 19:53:29 +0100

    Update FormAddContact.jsx

--- src/components/FormAddContact/FormAddContact.jsx
+++ src/components/FormAddContact/FormAddContact.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -9,52 +9,53 @@ const BtnStyled = styled.button`
   padding: 8px;
 `;
 
-export class FormAddContact extends Component {
-  state = {
-    name: '',
-    number: '',
+export const FormAddContact = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleInputChange = evt => {
+    const { value } = evt.target;
+    if (evt.target.name === 'name') {
+      setName(value);
+    } else if (evt.target.name === 'number') {
+      setNumber(value);
+    }
   };
 
-  handleInputChange = evt => {
-    const { name, value } = evt.target;
-    this.setState({ [name]: value });
-  };
-
-  handleFormSubmit = evt => {
+  const handleFormSubmit = evt => {
     evt.preventDefault();
-    this.props.onSubmit(this.state);
-    this.setState({ name: '', number: '' });
+    onSubmit({ name, number });
+    setName('');
+    setNumber('');
     Notify.success('The contact was created');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleFormSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            required
-            placeholder="Adam Smith"
-            value={this.state.name}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Number
-          <input
-            type="tel"
-            name="number"
-            required
-            pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
-            placeholder="000-00-00"
-            value={this.state.number}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <BtnStyled type="submit">Add contact</BtnStyled>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          required
+          placeholder="Adam Smith"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          name="number"
+          required
+          pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+          placeholder="000-00-00"
+          value={number}
+          onChange={handleInputChange}
+        />
+      </label>
+      <BtnStyled type="submit">Add contact</BtnStyled>
+    </form>
+  );
+};
